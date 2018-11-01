8690c569feb4012a0e33103fa913be2094c5e641
Belroguim <miguel.vilarinho@gmail.com>
2018-11-01 21:17:58 +0100

    Añado configuracion de fps y resolucion. Falla la exposicion aun. Luego habrá que enviar estos param a JSON

--- Examples/example_cmake/opencv_example.cpp
+++ Examples/example_cmake/opencv_example.cpp
@@ -9,6 +9,8 @@
 using namespace cv;
 using namespace std;
 
+#define n_Frames 100
+
 void drawText(Mat & image);
 
 int main()
@@ -17,10 +19,36 @@ int main()
     unsigned long dt_us_ul;
     float dt_ms_fl;
     
+    // La configuracion debemos cargarla con JSON
+    // http://www.raspberry-projects.com/pi/programming-in-c/json/rapidjson
+    // https://www.raspberrypi.org/forums/viewtopic.php?t=129950
+    
     cout << "Built with OpenCV " << CV_VERSION << endl;
     Mat image;
     VideoCapture capture;
     capture.open(0);
+    //modos: 320 x 240, 1280 x 920, 2592 x 1944);
+    // https://raspberrypi.stackexchange.com/questions/75120/increasing-fps-of-pi-camera-using-opencv-c-implementation
+    // https://stackoverflow.com/questions/11420748/setting-camera-parameters-in-opencv-python
+    capture.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    int Width = capture.get(CV_CAP_PROP_FRAME_WIDTH);
+    std::cout <<"FRAME_WIDTH (px) : "<< Width << endl;
+    capture.set(CV_CAP_PROP_FRAME_HEIGHT,420);
+    int Height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
+    std::cout <<"FRAME_HEIGHT (px): "<< Height << endl;
+    capture.set(CV_CAP_PROP_FPS,50);
+    int fps = capture.get(CV_CAP_PROP_FPS);
+    std::cout <<"FRAME_RATE (fps) : "<< fps << endl; 
+    capture.set(CV_CAP_PROP_BRIGHTNESS,0); 
+    int Brightness = capture.get(CV_CAP_PROP_BRIGHTNESS);
+    std::cout <<"BRIGHTNESS       : "<< Brightness << endl;
+/*    capture.set(CV_CAP_PROP_AUTO_EXPOSURE,0); 
+    int Auto_Exposure = capture.get(CV_CAP_PROP_AUTO_EXPOSURE);
+    std::cout <<"AUTO_EXPOSURE    : "<< Auto_Exposure << endl;
+    capture.set(CV_CAP_PROP_EXPOSURE,10000); 
+    int Exposure = capture.get(CV_CAP_PROP_EXPOSURE);
+    std::cout <<"EXPOSURE         : "<< Exposure << endl;*/
+
     if(capture.isOpened())
     {
         cout << "Capture is opened" << endl;
@@ -29,16 +57,11 @@ int main()
          if(image.empty())
                 cout << "Fallo en primera imagen" << endl;
         
-        for(int i=0; i<100; i+=1)
+        for(int i=0; i<n_Frames; i+=1)
         {
-            capture >> image;
-            cout << i << endl;
-            
+            capture >> image;      
             if(image.empty())
                 break;
-            
-//	    if(waitKey(10) >= 0)
-//                break;
         }
         cout << "Capture has finished" << endl;
         imwrite("image.jpg",image);
@@ -47,23 +70,11 @@ int main()
         dt_us_ul = dt.tv_sec*1e6 + dt.tv_usec;
         dt_ms_fl = (float)dt_us_ul/1000;
         cout <<"Elapsed time (ms):" << dt_ms_fl << endl;
+        cout <<"fps: "<< n_Frames*1000/dt_ms_fl << endl;
     }
     else
     {
         cout << "No capture" << endl;
-//        image = Mat::zeros(480, 640, CV_8UC1);
-//        drawText(image);
-//        imshow("Sample", image);
-//        waitKey(0);
     }
     return 0;
-}
-
-void drawText(Mat & image)
-{
-    putText(image, "Hello OpenCV",
-            Point(20, 50),
-            FONT_HERSHEY_COMPLEX, 1, // font face and scale
-            Scalar(255, 255, 255), // white
-            1, LINE_AA); // line thickness and type
 }
